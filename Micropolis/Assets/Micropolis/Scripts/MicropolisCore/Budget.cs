047a662c7b683ce929863bdcf9ee7f8b2991c593
Bil Simser <bsimser@shaw.ca>
2018-02-19 19:45:54 -0700

    stubbed buget functions

---
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Budget.cs
@@ -0,0 +1,43 @@
+﻿namespace Micropolis.MicropolisCore
+{
+    public partial class Micropolis
+    {
+        public void initFundingLevel()
+        {
+        }
+
+        /// <summary>
+        /// Game decided to show the budget window
+        /// </summary>
+        public void doBudget()
+        {
+        }
+
+        /// <summary>
+        /// User queried the budget window
+        /// </summary>
+        public void doBudgetFromMenu()
+        {
+        }
+
+        /// <summary>
+        /// Handle budget window.
+        /// </summary>
+        /// <param name="fromMenu">User requested the budget window</param>
+        public void doBudgetNow(bool fromMenu)
+        {
+        }
+
+        public void updateBudget()
+        {
+        }
+
+        public void showBudgetWindowAndStartWaiting()
+        {
+        }
+
+        public void setCityTax(short tax)
+        {
+        }
+    }
+}
