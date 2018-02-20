7186fb5f2c38d5cff221441426e05b76bb461eb7
Bil Simser <bsimser@shaw.ca>
2018-02-19 21:47:35 -0700

    stubbed out init routines

---
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Initialize.cs
@@ -0,0 +1,26 @@
+﻿namespace Micropolis.MicropolisCore
+{
+    public partial class Micropolis
+    {
+        /// <summary>
+        /// Reset many game state variables
+        /// </summary>
+        public void initWillStuff()
+        {
+        }
+
+        /// <summary>
+        /// Reset all maps in the simulator.
+        /// </summary>
+        public void resetMapState()
+        {
+        }
+
+        /// <summary>
+        /// Reset all tools in the simulator editor.
+        /// </summary>
+        public void resetEditorState()
+        {
+        }
+    }
+}
