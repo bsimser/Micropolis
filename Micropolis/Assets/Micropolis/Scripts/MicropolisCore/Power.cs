6c83f2ecb50a7cfbcd0c95c288a9d0b405d76957
Bil Simser <bsimser@shaw.ca>
2018-02-19 19:46:52 -0700

    stubbed power functions

--- Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Power.cs
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Power.cs
@@ -2,6 +2,10 @@
 {
     public partial class Micropolis
     {
+        /// <summary>
+        /// Scan the map for powered tiles, and copy them to the powerGridMap array.
+        /// Also warns the user about using too much power ('buy another power plant').
+        /// </summary>
         public void doPowerScan()
         {
         }
