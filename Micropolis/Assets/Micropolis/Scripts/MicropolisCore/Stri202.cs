de47a1b56fa2a147010993b486e9ac816e23732f
Bil Simser <bsimser@shaw.ca>
2018-02-19 20:52:10 -0700

    new enums for text messages

---
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Stri202.cs
@@ -0,0 +1,33 @@
+﻿namespace Micropolis.MicropolisCore
+{
+    /// <summary>
+    /// String numbers of score card.
+    /// </summary>
+    public enum Stri202
+    {
+        STR202_POPULATIONDENSITY_LOW = 0, // Low
+        STR202_POPULATIONDENSITY_MEDIUM = 1, // Medium
+        STR202_POPULATIONDENSITY_HIGH = 2, // High
+        STR202_POPULATIONDENSITY_VERYHIGH = 3, // Very High
+
+        STR202_LANDVALUE_SLUM = 4, // Slum
+        STR202_LANDVALUE_LOWER_CLASS = 5, // Lower Class
+        STR202_LANDVALUE_MIDDLE_CLASS = 6, // Middle Class
+        STR202_LANDVALUE_HIGH_CLASS = 7, // High
+
+        STR202_CRIME_NONE = 8, // Safe
+        STR202_CRIME_LIGHT = 9, // Light
+        STR202_CRIME_MODERATE = 10, // Moderate
+        STR202_CRIME_DANGEROUS = 11, // Dangerous
+
+        STR202_POLLUTION_NONE = 12, // None
+        STR202_POLLUTION_MODERATE = 13, // Moderate
+        STR202_POLLUTION_HEAVY = 14, // Heavy
+        STR202_POLLUTION_VERY_HEAVY = 15, // Very Heavy
+
+        STR202_GROWRATE_DECLINING = 16, // Declining
+        STR202_GROWRATE_STABLE = 17, // Stable
+        STR202_GROWRATE_SLOWGROWTH = 18, // Slow Growth
+        STR202_GROWRATE_FASTGROWTH = 19, // Fast Growth
+    }
+}
