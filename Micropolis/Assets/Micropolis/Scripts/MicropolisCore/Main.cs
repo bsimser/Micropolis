71003e4b25cd8bdd435ff8fc360279d5639dd357
Bil Simser <bsimser@shaw.ca>
2018-02-20 04:33:03 -0700

    stubbed out main functions

---
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Main.cs
@@ -0,0 +1,44 @@
+﻿namespace Micropolis.MicropolisCore
+{
+    public partial class Micropolis
+    {
+        public string getMicropolisVersion()
+        {
+            return "5.0";
+        }
+
+        /// <summary>
+        /// Locate resource directory.
+        /// </summary>
+        public void environmentInit()
+        {
+        }
+
+        /// <summary>
+        /// Initialize for a simulation
+        /// </summary>
+        public void simInit()
+        {
+        }
+
+        public void simUpdate()
+        {
+        }
+
+        public void simHeat()
+        {
+        }
+
+        public void simLoop(bool doSim)
+        {
+        }
+
+        public void simTick()
+        {
+        }
+
+        public void simRobots()
+        {
+        }
+    }
+}
