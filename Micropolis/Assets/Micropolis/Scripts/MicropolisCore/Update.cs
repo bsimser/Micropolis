de35ee960fe32d786054f06c7db1d3d63f109a63
Bil Simser <bsimser@shaw.ca>
2018-02-19 20:33:29 -0700

    stubbed out update functions

---
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Update.cs
@@ -0,0 +1,64 @@
+﻿namespace Micropolis.MicropolisCore
+{
+    public partial class Micropolis
+    {
+        public void doUpdateHeads()
+        {
+        }
+
+        public void updateMaps()
+        {
+        }
+
+        public void updateGraphs()
+        {
+        }
+
+        public void updateEvaluation()
+        {
+        }
+
+        public void updateHeads()
+        {
+        }
+
+        /// <summary>
+        /// Set a flag that the funds display is out of date.
+        /// </summary>
+        public void updateFunds()
+        {
+        }
+
+        public void reallyUpdateFunds()
+        {
+        }
+
+        public void doTimeStuff()
+        {
+        }
+
+        public void updateDate()
+        {
+        }
+
+        public void showValves()
+        {
+        }
+
+        public void drawValve()
+        {
+        }
+
+        public void setDemand(float r, float c, float i)
+        {
+        }
+
+        public void updateOptions()
+        {
+        }
+
+        public void updateUserInterface()
+        {
+        }
+    }
+}
