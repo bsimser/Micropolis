8591764402521dbec384c3c48b2ee226d3211dca
Bil Simser <bsimser@shaw.ca>
2018-02-24 12:29:20 -0700

    added in dynamic tilesets

--- Micropolis/Assets/Micropolis/Scripts/MicropolisGame/TileEngine.cs
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisGame/TileEngine.cs
@@ -17,6 +17,22 @@ namespace MicropolisGame
 
             var path = string.Format("Tiles/tiles_{0}", tileId);
             var asset = Resources.Load<Tile>(path);
+
+            // dynamic load the sprite for the tile based on our theme
+            //var theme = "x11";
+            //var theme = "classic";
+            var theme = "classic95";
+            //var theme = "wildwest";
+            //var theme = "mooncolony";
+            //var theme = "medievaltimes";
+            //var theme = "futureusa";
+            //var theme = "futureeurope";
+            //var theme = "ancientasia";
+
+            // TODO we don't have to load all the sprites every time
+            Sprite[] sprites = Resources.LoadAll<Sprite>("Tilesets/" + theme + "/tiles");
+            asset.sprite = sprites[tileId];
+
             _tiles.Add(tileId, asset);
             return _tiles[tileId];
         }
