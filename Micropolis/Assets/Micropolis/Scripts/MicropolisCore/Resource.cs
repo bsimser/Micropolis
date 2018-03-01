ad5ec3a74040472179912ec90cc0a78aa005179b
Bil Simser <bsimser@shaw.ca>
2018-02-28 19:55:09 -0700

    file rename for partial class

--- Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Resource.cs
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Resource.cs
@@ -1,21 +1,5 @@
-﻿namespace MicropolisCore
+namespace MicropolisCore
 {
-    /// <summary>
-    /// Get resources (from files)
-    /// </summary>
-    public partial class Micropolis
-    {
-        /// <summary>
-        /// Get the text of a message.
-        /// </summary>
-        /// <param name="str">Destination of the text (usually 256 characters long)</param>
-        /// <param name="id">Identification of the resource</param>
-        /// <param name="num">Number of the string in the resource</param>
-        public void getIndString(string str, int id, short num)
-        {
-        }
-    }
-
     /// <summary>
     /// Resource of the game (a file with data loaded in memory).
     /// </summary>
--- Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Scenario.cs
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisCore/Scenario.cs
@@ -1,24 +1,5 @@
 ﻿namespace MicropolisCore
 {
-    /// <summary>
-    /// Available scenarios
-    /// </summary>
-    public enum ScenarioType
-    {
-        SC_NONE,           // No scenario (free playing)
-
-        SC_DULLSVILLE,     // Dullsville (boredom)
-        SC_SAN_FRANCISCO,  // San francisco (earthquake)
-        SC_HAMBURG,        // Hamburg (fire bombs)
-        SC_BERN,           // Bern (traffic)
-        SC_TOKYO,          // Tokyo (scary monster)
-        SC_DETROIT,        // Detroit (crime)
-        SC_BOSTON,         // Boston (nuclear meltdown)
-        SC_RIO,            // Rio (flooding)
-
-        SC_COUNT,          // Number of scenarios  
-    }
-
     public class Scenario
     {
         public ScenarioType id;
