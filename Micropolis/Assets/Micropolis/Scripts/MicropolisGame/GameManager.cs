ecbdcea66eda6a5a484475ea83d235d271fdfe5c
Bil Simser <bsimser@shaw.ca>
2018-03-11 07:22:16 -0600

    updated to unity 2017.3.1f1

--- Micropolis/Assets/Micropolis/Scripts/MicropolisGame/GameManager.cs
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisGame/GameManager.cs
@@ -17,18 +17,28 @@ namespace MicropolisGame
         {
             // create an instance of the Micropolis engine
             _engine = MicropolisUnityEngine.CreateUnityEngine();
-
             // setup our internal manager classes
             _tileManager = new TileManager(_engine);
             _uiManager = new UIManager(_engine);
-
-            // TODO until we get a front end let's kick off the engine here with some hard coded values
-            //GenerateRandomCity();
-            LoadCity();
-            //LoadScenario();
         }
 
-        private void GenerateRandomCity()
+        private void Update()
+        {
+            var ellapsedMilliseconds = (int) (Time.deltaTime * 1000);
+            if (ellapsedMilliseconds % 16 == 0)
+            {
+                // TODO should the check for speed be outside of this if?
+                if (_engine.simSpeed != 0)
+                {
+                    _engine.tickEngine();
+                    _tileManager.Draw();
+                }
+            }
+        }
+
+        // TODO maybe move all UI functions below to it's own class or the UI Manager?
+
+        public void GenerateRandomCity()
         {
             _engine.initGame();
             _engine.simInit();
@@ -38,7 +48,7 @@ namespace MicropolisGame
             _engine.setEnableDisasters(false);
         }
 
-        private void LoadCity()
+        public void LoadCity()
         {
             _engine.initGame();
             _engine.simInit();
@@ -48,19 +58,44 @@ namespace MicropolisGame
             _engine.setEnableDisasters(false);
         }
 
-        private void LoadScenario()
+        public void LoadScenario()
         {
             _engine.loadScenario(ScenarioType.SC_DULLSVILLE);
         }
 
-        private void Update()
+        public void Quit()
         {
-            var ellapsedMilliseconds = (int) (Time.deltaTime * 1000);
-            if (ellapsedMilliseconds % 16 == 0)
-            {
-                _engine.tickEngine();
-                _tileManager.Draw();
-            }
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        public void Pause()
+        {
+            _engine.pause();
+            // TODO show the pause panel
+        }
+
+        public void QuitToMain()
+        {
+            // TODO hide this panel
+            // TODO end the simulator
+            // TODO show main panel
+        }
+
+        public void Resume()
+        {
+            // TODO hide the pause panel
+            _engine.resume();
+        }
+
+        public void LoadGraphics()
+        {
+            // TODO show a list of graphic tilesets
+            // TODO reload the tileset with the new graphics
+            // TODO resume the game (or just come back to the pause panel?)
         }
     }
 }
